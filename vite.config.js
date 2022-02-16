059a5bf0def6497db81d89911bdb04680af907e3
Migue Martelo <miguemartelo1031@gmail.com>
2022-02-15 20:07:18 -0500

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://miguemartelo.github.io/',
+  base: 'https://miguemartelo.github.io/gh-react-vite/',
   plugins: [react()],
 });
